219d086e49af7d3058e6db45c20e0fa1176442d9
Jeremy Sovran <jeremysovran@gmail.com>
2018-12-06 19:13:45 +0100

    final commit

---
+++ app.js
@@ -0,0 +1,19 @@
+window.addEventListener('keydown', function(e) {
+  var audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  var key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  // console.log(audio);
+  // console.log(key);
+  if (!audio) return;
+  audio.currentTime = 0;
+  audio.play();
+  key.classList.add('playing');
+
+  var keys = document.querySelectorAll('.key');
+
+  keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+});
+
+function removeTransition(e) {
+  if (e.propertyName !== 'transform') return;
+  this.classList.remove('playing');
+}
